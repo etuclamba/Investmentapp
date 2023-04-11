c04725d251562f20f275c2d1032d0b5f1320123f
ETUC <127153313+etuclamba@users.noreply.github.com>
2023-04-11 22:58:16 +0100

    Update invest.py

--- investment-app/invest.py
+++ investment-app/invest.py
@@ -6,7 +6,7 @@ investor_name = input("Enter your name: ")
 amount_to_invest = float(input("Enter amount to invest: "))
 
 # Calculate wallet balance
-wallet_balance = amount_to_invest * 1.5 #wallet_ballance = (amount_to_invest * 0.5) + amount_to_invest
+wallet_balance  = (amount_to_invest * 0.5) + amount_to_invest
 print("Wallet balance: ", wallet_balance)
 
 # Allow investor to withdraw funds
@@ -19,4 +19,4 @@ else:
     vat_deduction = 0.07 * wallet_balance
     wallet_balance -= (amount_to_withdraw + vat_deduction)
     print("Withdrawal successful, your account has been debited with", amount_to_withdraw, "and 7% VAT has been deducted.")
-    print("Available balance: ",wallet_balance)
+    print("Final balance: ", wallet_balance)
